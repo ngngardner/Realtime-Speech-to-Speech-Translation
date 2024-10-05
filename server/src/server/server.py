a5f07cb834198ccd49a766e47e2e0a4e9d1a4f24
Noah Gardner <ngngardner@gmail.com>
2024-10-05 14:25:46 -0400

    linter fixes

--- server/src/server/server.py
+++ server/src/server/server.py
@@ -1,9 +1,8 @@
-"""Server for real-time translation and voice synthesization"""
+"""Server for real-time translation and voice synthesization."""
 
 import select
 import socket
 from queue import Queue
-from typing import Dict
 
 import pyaudio
 import torch
@@ -16,7 +15,8 @@ console = Console()
 
 
 class AudioSocketServer:
-    """Class that handles real-time translation and voice synthesization
+    """Class that handles real-time translation and voice synthesization.
+
     Socket input -> SpeechRecognition -> text -> TextToSpeech -> Socket output
     """
 
@@ -29,14 +29,15 @@ class AudioSocketServer:
     #   For socket.listen()
     BACKLOG = 5
 
-    def __init__(self, whisper_model):
+    def __init__(self, whisper_model: str = "base") -> None:
+        """Initialize the server."""
         self.audio = pyaudio.PyAudio()
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Let kernel know we want to reuse the same port for restarting the server
-        #   in quick succession
+        # Let kernel know we want to reuse the same port for restarting the
+        #   server in quick succession
         self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # TODO: For multiple concurrent users we will need more queues
-        #   for now we only want one user to work first
+        # TODO: For multiple concurrent users we will need more queues for now
+        #   we only want one user to work first
         self.data_queue: Queue = Queue()
 
         # Initialize the transcriber model
@@ -52,32 +53,38 @@ class AudioSocketServer:
         self.text_to_speech.load_speaker_embeddings()
         self.read_list = []
 
-    def __del__(self):
+    def __del__(self) -> None:
+        """Cleanup function for the server."""
         self.audio.terminate()
         self.transcriber.stop()
         self.serversocket.shutdown()
         self.serversocket.close()
 
-    def handle_generation(self, packet: dict):
-        """Placeholder function for transcription"""
+    def handle_generation(self, packet: dict) -> None:
+        """Transcription placeholder."""
 
-    def handle_transcription(self, packet: str, client_socket):
-        """Callback function to put finalized transcriptions into TTS"""
+    def handle_transcription(self, packet: str, client_socket: socket.socket) -> None:
+        """Finalize transcriptions into TTS."""
         console.log(f"Added {packet} to synthesize task queue")
         self.text_to_speech.synthesise(packet, client_socket)
 
-    def handle_synthesize(self, audio: torch.Tensor, client_socket):
-        """Callback function to stream audio back to the client"""
+    def handle_synthesize(
+        self,
+        audio: torch.Tensor,
+        client_socket: socket.socket,
+    ) -> None:
+        """Stream audio back to the client."""
         self.stream_numpy_array_audio(audio, client_socket)
 
-    def start(self):
-        """Starts the server"""
+    def start(self) -> None:
+        """Start the server."""
         self.transcriber.start(16000, 2)
         console.log(f"Listening on port {self.PORT}")
         self.serversocket.bind(("", self.PORT))
         self.serversocket.listen(self.BACKLOG)
-        # Contains all of the socket connections, the first is the server socket for listening to
-        #   new connections. All other ones are for individual clients sending data.
+        # Contains all of the socket connections, the first is the server socket
+        #   for listening to new connections. All other ones are for individual
+        #   clients sending data.
         self.read_list = [self.serversocket]
 
         try:
@@ -109,8 +116,12 @@ class AudioSocketServer:
         self.serversocket.close()
         console.log("Sockets cleaned up")
 
-    def stream_numpy_array_audio(self, audio, client_socket):
-        """Streams audio back to the client"""
+    def stream_numpy_array_audio(
+        self,
+        audio: torch.Tensor,
+        client_socket: socket.socket,
+    ) -> None:
+        """Streams audio back to the client."""
         if client_socket is None:
             console.log("Error: client_socket is None")
             return
@@ -122,7 +133,8 @@ class AudioSocketServer:
                 self.read_list.remove(client_socket)
 
 
-def main():
+def main() -> None:
+    """Start the server."""
     server = AudioSocketServer(whisper_model="base")
     server.start()
 
